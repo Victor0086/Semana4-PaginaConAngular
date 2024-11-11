abc0b2c562d89b8e5084d7d0f22d10d6fb375ec8
Victor0086 <victor_retamal@outlook.cl>
2024-11-11 13:51:54 -0300

    actualizacion

--- src/app/components/registro/registro.component.ts
+++ src/app/components/registro/registro.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule], 
   templateUrl: './registro.component.html',
-  styleUrl: './registro.component.css'
+  styleUrls: ['./registro.component.css']
 })
 export class RegistroComponent {
+  nombreCompleto = '';
+  nombreUsuario = '';
+  email = '';
+  password = '';
+  confirmPassword = '';
+  fechaNacimiento: string = '';
+  direccion = '';
 
+  categorias = ['Acción', 'Terror', 'Aventura', 'Deporte'];
+  usuarioRegistrado = false;
+
+  // para manejar el registro
+  registrarUsuario(): void {
+    if (this.password !== this.confirmPassword) {
+      alert('Las contraseñas no coinciden');
+      return;
+    }
+
+    // Asigna el nombre de usuario al iniciar sesión
+    this.nombreUsuario = this.nombreUsuario || 'Usuario Desconocido';
+
+    // Lógica para enviar datos a un servicio de registro
+    alert('Registro exitoso');
+    this.usuarioRegistrado = true;
+  }
 }
